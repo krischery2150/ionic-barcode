286a0d583feb728c0b39fa88b3bc35079a5146cf
Kris Chery <kris.chery2@gmail.com>
2018-04-13 00:51:40 -0500

    ios version worked

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { BarcodeScanner, BarcodeScannerOptions } from '@ionic-native/barcode-scanner';
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
+  options: BarcodeScannerOptions;
+  constructor(public navCtrl: NavController, public barcodeScanner: BarcodeScanner) {
 
   }
+
+scan(){
+    this.barcodeScanner.scan().then((barcodeData) => {
+   console.log('Barcode data', barcodeData);
+  }).catch(err => {
+      console.log('Error', err);
+  });
+}
 
 }
